44afe11416bf952b233ce57cfb3e1dde0be10a38
AlanFRomo <alangino55@live.com>
2020-06-18 19:09:33 -0300

    Clase USB creada

---
+++ Usb.java
@@ -0,0 +1,32 @@
+/**
+ * Usb
+ */
+public class Usb extends Producto {
+    private String color;
+    private int memoria;
+    
+    public Usb(int cod, String nombre, String marca, int stock, double precio, String color, int memoria){
+        super(cod, nombre, marca, stock, precio);
+        this.color = color;
+        this.memoria = memoria;
+    }
+
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public int getMemoria(){
+        return memoria;
+    }
+    public void setMemoria(int memoria){
+        this.memoria = memoria;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nColor: "+getColor() + "\nMemoria: "+ getMemoria();
+    }
+}
