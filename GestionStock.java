5e02126a00e600c2853b62ae7b03ab97cf564cae
Fran0621 <francabrlen@gmail.com>
2020-06-18 17:47:12 -0300

    Dejo inciado el array asi pueden hacer los metodos tranquilos

--- GestionStock.java
+++ GestionStock.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * GestionStock
+ */
 public class GestionStock {
 
-    
-}
+    private ArrayList<Producto> listaProductos;
+
+    public GestionStock(){
+        listaProductos = new ArrayList<>();
+    }
